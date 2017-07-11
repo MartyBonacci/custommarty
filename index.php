6671fd2be32bfed6f24cb75d76b67d23738d576d
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 21:01:45 -0600

    create custommarty

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-image: url("/images/marty-on-mt-taylor.jpg");
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Mass-Customization with Marty Bonacci</h1>
+		<br>
+		<p>I believe that you are a unique person with a unique body, unique preferences, passions, dislikes, and feelings...and that you deserve to be treated like one of a kind and have products made just for you that make your life even more awsome! </p>
+	</body>
+</html>
