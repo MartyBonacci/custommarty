39e2d05752e1d18046fa50ac3a592d60eb2fb448
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 21:54:59 -0600

    text edit 2

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<h1 align="right">Mass-Customization with Marty Bonacci</h1>
 		<br>
 		<p align="right">I believe that you are a unique person with a unique body, unique preferences, passions,</p>
-		<p align="right">thoughts, and feelings. You deserve to be treated like one of a kind and</p>
-		<p align="right">have products made just for you that make your life even more awsome! </p>
+		<p align="right">thoughts, and feelings. You deserve to be treated like one of a kind and have</p>
+		<p align="right">products made just for you that make your life even more awsome! </p>
 	</body>
 </html>
